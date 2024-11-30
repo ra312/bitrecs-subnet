99addf5f2d63aefd4b74d6de11278ba389771961
dmose <100089505+janusdotai@users.noreply.github.com>
2024-11-30 16:16:50 -0500

    latest

--- template/base/validator.py
+++ template/base/validator.py
@@ -246,7 +246,7 @@ class BaseValidatorNeuron(BaseNeuron):
                         #chosen_uids = [0]
 
                         chosen_uids = available_uids
-                        np.append(chosen_uids, 1)
+                        np.append(chosen_uids, [1])
                         
                         bt.logging.debug(f"len(chosen_uids): {len(chosen_uids)}")
                         bt.logging.debug(f"chosen_uids: {chosen_uids}")
