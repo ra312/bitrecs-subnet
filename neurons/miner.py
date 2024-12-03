1e07ee2e68bc63ad0e80e159fdc5d60b8f34a57f
dmose <100089505+janusdotai@users.noreply.github.com>
2024-12-03 11:20:26 -0500

    latest

--- neurons/miner.py
+++ neurons/miner.py
@@ -144,7 +144,8 @@ class Miner(BaseMinerNeuron):
                 #model = "llama3.1:70b" //slow
                 #model = "llama3.1:70b-instruct-q4_0" //slow
                 #model = "qwen2.5:32b" //invalid results
-                model = "qwen2.5:32b-instruct"
+                #model = "qwen2.5:32b-instruct"
+                model = "qwq"
             case LLM.OPEN_ROUTER:
                 #model = "google/gemini-flash-1.5-8b"
                 model = "meta-llama/llama-3.1-70b-instruct:free"
