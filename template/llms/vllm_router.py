aaee0e5c0df7fb3abc685d8ca768a382e5fe51ec
dmose <100089505+janusdotai@users.noreply.github.com>
2024-12-02 23:39:22 -0500

    latest

--- template/llms/vllm_router.py
+++ template/llms/vllm_router.py
@@ -8,27 +8,29 @@ class vLLM:
                  model="NousResearch/Meta-Llama-3-8B-Instruct", 
                  system_prompt="You are a helpful AI assistant.", 
                  temp=0.0):
-        self.key = "abc123H"
+        self.key = "token-abc123"
         self.model = model
         self.system_prompt = system_prompt
         self.temp = temp
 
 
     def call_vllm(self, user_prompt):
-        headers = {
-            "Authorization": "Bearer {}".format(self.key)
-        }
-        data = {
-            "prompt": "{}".format(user_prompt),
-            "max_tokens": 500
-        }
-        response = requests.post("http://0.0.0.0:8000/v1/completions", json=data, headers=headers)
         
-        llm_response = response.json()
-        bt.logging.trace("vLLM response: {}".format(llm_response))
+        client = OpenAI(
+            base_url="http://localhost:8000/v1",
+            api_key="token-abc123",
+        )
 
-        return llm_response["choices"][0]["text"].strip()  # Strip to remove any extraneous whitespace
-                
+        completion = client.chat.completions.create(
+            model="NousResearch/Meta-Llama-3-8B-Instruct",
+            messages=[
+                {"role": "user", "content": user_prompt}
+            ]
+        )
+        print(completion.choices[0].message)
+
+        return completion.choices[0].message
+
     
     def call_vllm2(self, user_prompt):        
         openai_api_key = self.key
