f1a391dd705a180c2e0d4175d44ed17847a3626d
dmose <100089505+janusdotai@users.noreply.github.com>
2024-12-03 19:15:37 -0500

    latest

--- template/llms/vllm_router.py
+++ template/llms/vllm_router.py
@@ -11,24 +11,17 @@ class vLLM:
                  key, 
                  model="NousResearch/Meta-Llama-3-8B-Instruct", 
                  system_prompt="You are a helpful AI assistant.", 
-                 temp=0.0):
-        
+                 temp=0.0):        
         self.VLLM_API_KEY = key
         if not self.VLLM_API_KEY:
-            raise ValueError("VLLM_API_KEY is not set")
-        
+            raise ValueError("VLLM_API_KEY is not set")        
         self.model = model
         self.model = model
         self.system_prompt = system_prompt
-        self.temp = temp
-        #self.key = "token-abc123H"
+        self.temp = temp       
 
 
     def call_vllm(self, user_prompt) -> str:
-        
-        bt.logging.info(f"Calling vLLM with user prompt: {user_prompt}")
-        bt.logging.info(f"key: {self.VLLM_API_KEY}")
-
         client = OpenAI(
             base_url="http://localhost:8000/v1",
             api_key=self.VLLM_API_KEY,
