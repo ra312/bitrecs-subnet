678b298db2c0e577ef3fa3ff1dc9f9adbc4cf061
dmose <100089505+janusdotai@users.noreply.github.com>
2024-12-08 10:57:25 -0500

    latest

--- neurons/validator.py
+++ neurons/validator.py
@@ -71,7 +71,7 @@ class Validator(BaseValidatorNeuron):
         return await forward(self, pr)    
   
     @execute_periodically(timedelta(seconds=30))
-    def validator_callback(self):
+    async def validator_callback(self):
         bt.logging.trace(f"\033[1;32m Validator back loop ran at {int(time.time())}. \033[0m")
         #bt.logging.trace(f"last block {self.subtensor.block} on step {self.step} ")
         
