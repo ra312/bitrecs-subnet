b775993744d44290740d8377254aa964e4a31a7b
dmose <100089505+janusdotai@users.noreply.github.com>
2024-12-12 09:23:42 -0500

    latest

--- template/api/utils.py
+++ template/api/utils.py
@@ -107,7 +107,7 @@ async def check_server_status(ip, port, timeout=3) -> bool:
             return False
         
         #key = list(api_key_info["keys"].keys())[0]
-        key = next(iter(api_key_info["keys"]))
+        key = str(next(iter(api_key_info["keys"])))
 
         r = requests.get(f"http://{ip}:{port}/ping", 
                          timeout=timeout,
