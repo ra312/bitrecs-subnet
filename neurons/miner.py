6fc57cd419331814681239d38cdf90b25cf3acde
dmose <100089505+janusdotai@users.noreply.github.com>
2024-12-16 14:31:10 -0500

    latest

--- neurons/miner.py
+++ neurons/miner.py
@@ -195,7 +195,7 @@ class Miner(BaseMinerNeuron):
                 continue
             dictionary_item["name"] = dictionary_item["name"].replace("'", "-")  # Remove single quotes
             recommendation = str(dictionary_item)
-            final_results.append(recommendation)
+            final_results.append(recommendation)        
       
         output_synapse=BitrecsRequest(
             name=synapse.name, 
@@ -210,7 +210,7 @@ class Miner(BaseMinerNeuron):
             results=final_results,
             models_used=[self.model],
             miner_uid=str(self.uid),
-            miner_hotkey=self.wallet.hotkey,
+            miner_hotkey=self.metagraph.hotkeys[self.uid]
         )
         
         bt.logging.info(f"MINER {self.uid} FORWARD PASS RESULT -> {output_synapse}")
