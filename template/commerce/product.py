5c2c857bc080289a6e8bfa643dc476628d96fd98
dmose <100089505+janusdotai@users.noreply.github.com>
2024-12-17 09:55:10 -0500

    adding tests

--- template/commerce/product.py
+++ template/commerce/product.py
@@ -1,7 +1,15 @@
 
 import json
-from dataclasses import dataclass
 import bittensor as bt
+from dataclasses import dataclass
+from enum import Enum
+
+
+class CatalogProvider(Enum):
+    SHOPIFY = 1
+    AMAZON = 2
+    WOOCOMMERCE = 3
+    BIGCOMMERCE = 4
 
 
 @dataclass
@@ -10,12 +18,75 @@ class Product:
     name: str
     price: float
 
+
     @staticmethod
     def try_parse_context(context: str) -> list["Product"]:
+        """
+        Default converter expects a json array of products with sku/name/price fields
+
+        """
         try:
             store_catalog: list[Product] = json.loads(context)
             return store_catalog
         except Exception as e:
             bt.logging.error(f"try_parse_context Exception: {e}")
             return []
+        
+        
+    @staticmethod
+    def convert(context: str, provider: CatalogProvider) -> list["Product"]:
+        """
+            Generaly context should be the store catalog in the Product format
+            If its not in bitrecs format and from a known provider, we can convert it
+
+        """
+        match provider:
+            case CatalogProvider.SHOPIFY:
+                return ShopifyConverter().convert(context)                
+            case CatalogProvider.AMAZON:
+                return AmazonConverter().convert(context)
+            case CatalogProvider.WOOCOMMERCE:
+                return WoocommerceConverter().convert(context)
+            case CatalogProvider.BIGCOMMERCE:
+                return BigcommerceConverter().convert(context)
+            case _:
+                raise NotImplementedError("invalid provider")
+
+
+
+class WoocommerceConverter:
     
+    def convert(self, context: str) -> list["Product"]:
+        return Product.try_parse_context(context)
+
+    
+class AmazonConverter:
+    
+    def convert(self, context: str) -> list["Product"]:
+        result : list[Product] = []
+        for p in json.loads(context):
+            try:
+                sku = p.get("asin")
+                if p["metadata"]:
+                    name = p["metadata"].get("title")
+                    price = p["metadata"].get("price")
+                if not sku or not name or not price:
+                    continue
+                result.append(Product(sku=sku, name=name, price=price))
+            except Exception as e:
+                bt.logging.error(f"AmazonConverter.convert Exception: {e}")
+                continue
+        return result
+    
+
+class ShopifyConverter:
+    
+    def convert(self, context: str) -> list["Product"]:
+        raise NotImplementedError("Shopify not implemented")
+    
+
+    
+class BigcommerceConverter:
+    
+    def convert(self, context: str) -> list["Product"]:
+        raise NotImplementedError("Bigcommerce not implemented")
