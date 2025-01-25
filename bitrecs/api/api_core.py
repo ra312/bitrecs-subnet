c2033ea1dbe9d02a8be9292bbfec30e5739cff9b
dmose <100089505+janusdotai@users.noreply.github.com>
2025-01-25 12:05:14 -0500

    limits

--- bitrecs/api/api_core.py
+++ bitrecs/api/api_core.py
@@ -9,17 +9,18 @@ def get_forwarded_for(request: Request):
     return request.headers.get("x-forwarded-for")
 
 limiter = Limiter(key_func=get_remote_address)
-
 #limiter = Limiter(key_func=get_forwarded_for)
 
 
-@limiter.limit("120/minute")
+@limiter.limit("60/minute")
 async def filter_allowed_ips(self, request: Request, call_next): 
      
     forwarded_for = request.headers.get("x-forwarded-for")
     if not forwarded_for:
+        bt.logging.trace(f"Forwarded for not found, get_remote_address ... ")
         forwarded_for = get_remote_address(request)
-    bt.logging.trace("Forwarded for:", forwarded_for)
+       
+    bt.logging.trace(f"Resolved to: {forwarded_for}")
 
     if 1==1:   
         if (
@@ -28,12 +29,14 @@ async def filter_allowed_ips(self, request: Request, call_next):
             and self.allowed_ips
         ):
             print("Blocking an unallowed ip:", forwarded_for, flush=True)
+            bt.logging.error(f"Blocked IP: {forwarded_for}")
             return Response(
                 content="You do not have permission to access this resource",
                 status_code=403,
             )
-        print("Allow an ip:", forwarded_for, flush=True)
-
+        
+    #print("Allow an ip:", forwarded_for, flush=True)
+    bt.logging.trace(f"Allowed IP {forwarded_for}")
     response = await call_next(request)
     return response
 
