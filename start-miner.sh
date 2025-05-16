a1a29cdc30823debcb618501cf08d5c75af2e2a2
Greg Schwartz <greg@gregschwartz.net>
2025-05-16 10:29:04 -0700

    Better start scripts

--- start-miner.sh
+++ start-miner.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
-NETUID=60 # Default to mainnet
 
-# Start miner and save PID
-python -m neurons.miner --netuid $NETUID --subtensor.chain_endpoint finney \
+# Default to production environment
+ENV="mainnet"
+NETUID=60
+NETWORK="finney"
+
+if [ "$1" == "--test" ]; then
+    ENV="testnet"
+    NETUID=350
+    NETWORK="test"
+fi
+
+echo "Starting miner in $ENV environment with netuid $NETUID"
+python -m neurons.miner --netuid $NETUID \
+    --subtensor.chain_endpoint $NETWORK --subtensor.network $NETWORK \
     --wallet.name miner --wallet.hotkey default \
     --axon.port 8092 --axon.external_port 8092 \
     --logging.debug
