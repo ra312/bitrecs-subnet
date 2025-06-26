a93330ea611803a79fa3dda3073b7e5a34b30966
Greg Schwartz <greg@gregschwartz.net>
2025-06-26 16:40:13 -0700

    We don't use a virtual environment on production, so only activate the virtual environment if it exists

--- start-miner.sh
+++ start-miner.sh
@@ -5,6 +5,7 @@ ENV="mainnet"
 NETUID=60
 NETWORK="finney"
 PORT=9090  # Default port
+COMMAND_WITH_PATH="python3"
 
 if [ "$1" == "--test" -o "$1" == "--testnet" ] || [ "$1" == "--testnet" ]; then
     ENV="testnet"
@@ -12,11 +13,15 @@ if [ "$1" == "--test" -o "$1" == "--testnet" ] || [ "$1" == "--testnet" ]; then
     NETWORK="test"
 fi
 
-# Activate virtual environment
-source venv/bin/activate
+# Activate virtual environment if it exists
+if [[ -d "venv" && -f "venv/bin/activate" ]]; then
+    echo "Activating virtual environment"
+    source venv/bin/activate
+    COMMAND_WITH_PATH="venv/bin/python3"
+fi
 
 echo "Starting miner in $ENV environment with netuid $NETUID on port $PORT"
-venv/bin/python3 -m neurons.miner --netuid $NETUID \
+$COMMAND_WITH_PATH -m neurons.miner --netuid $NETUID \
     --subtensor.chain_endpoint $NETWORK --subtensor.network $NETWORK \
     --wallet.name miner --wallet.hotkey default \
     --axon.port $PORT --axon.external_port $PORT \
--- start-validator.sh
+++ start-validator.sh
@@ -6,6 +6,7 @@ NETUID=60
 NETWORK="finney"
 PORT=8090  # Default port
 PROXY_PORT=10913 # Used on DigitalOcean
+COMMAND_WITH_PATH="python3"
 
 if [[ "$@" == *"--test"* || "$@" == *"--testnet"* ]]; then
     ENV="testnet"
@@ -18,12 +19,15 @@ if [[ "$@" == *"--proxy.port"* ]]; then
     PROXY_PORT=$(echo "$@" | grep -o -- "--proxy.port [0-9]*" | grep -o "[0-9]*")
 fi
 
-# Activate virtual environment
-echo "Activating virtual environment"
-source venv/bin/activate
+# Activate virtual environment if it exists
+if [[ -d "venv" && -f "venv/bin/activate" ]]; then
+    echo "Activating virtual environment"
+    source venv/bin/activate
+    COMMAND_WITH_PATH="venv/bin/python3"
+fi
 
 echo "Starting validator in $ENV environment with netuid $NETUID on port $PORT and proxy port $PROXY_PORT"
-venv/bin/python3 -m neurons.validator --netuid $NETUID \
+$COMMAND_WITH_PATH -m neurons.validator --netuid $NETUID \
     --subtensor.chain_endpoint $NETWORK --subtensor.network $NETWORK \
     --wallet.name validator --wallet.hotkey default \
     --axon.port $PORT --axon.external_port $PORT \
